526d9467c11642ea1ff46e3033fafa3006cdf1c0
Ahmad Thomthomi Barosimi <tommy.ethan.walter@gmail.com>
2024-05-16 16:48:09 +0700

    materi file upload CRUD MYSQL

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const database = require("./database");
+const userController = require("./controllers/userController");
+const errorhanlder = require("./middleware/errorHandler");
+const logger = require("./middleware/logger");
+const upload = require("./utils/upload");
+const multerError = require("./middleware/multerError");
+const userRoutes = require("./routes/userRoutes");
 
 const PORT = 8000;
 
@@ -11,7 +17,36 @@ app.use(express.json());
 // Menangangi data dari client atau browser
 app.use(express.urlencoded({ extended: true }));
 
-// ROUTE http://localhost:8000/
+// Manangani ERROR
+app.use(errorhanlder);
+
+app.use(logger);
+// // ROUTE http://localhost:8000/
+// // METHOD GET
+// app.get("/", (req, res) => {
+//   res.json({
+//     message: "Berhasil melakukan routing✨",
+//   });
+// });
+
+// // ROUTING Users
+// // Ambil data semua users
+// app.get("/api/users", userController.getAllUsers);
+
+// // METHOD POST MENAMBAHKAN DATA USER BARU
+// app.post("/api/users", userController.createNewUser);
+
+// // PUT METHOD Mengupdate data user sesuai dengan ID-nya
+// app.put("/api/users/:id", userController.updateUserById);
+
+// // METHOD DELETE untuk menghapus user
+// app.delete("/api/users/:id", userController.deleteUserById);
+
+// // METHOD GET dengan paramter id
+// app.get("/api/users/:id", userController.getUserById);
+
+// FILE UPLOAD
+
 // METHOD GET
 app.get("/", (req, res) => {
   res.json({
@@ -19,79 +54,9 @@ app.get("/", (req, res) => {
   });
 });
 
-// ROUTING Users
-// Ambil data semua users
-app.get("/api/users", (req, res) => {
-  database.query(`SELECT * FROM users`, (err, results) => {
-    if (err) {
-      res.status(500).json({ error: "something wrong" });
-      throw err;
-    }
-    console.log(results);
-    res.json({ results });
-  });
-});
-
-// METHOD POST MENAMBAHKAN DATA USER BARU
-app.post("/api/users", (req, res) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    return res.status(400).json({
-      error: "Silahkan isi field name, email, dan password!",
-    });
-  }
-  res.json({
-    name: name,
-    email: email,
-    password: password,
-    message: "User telah ditambahkan!",
-  });
-});
-
-// PUT METHOD Mengupdate data user sesuai dengan ID-nya
-app.put("/api/users/:id", (req, res) => {
-  const { id } = req.params;
-  const { name, email, password } = req.body;
-  if (!id || !name || !email || !password) {
-    return res.status(400).json({
-      error: "Silahkan isi field id, name, email, dan password!",
-    });
-  }
-  res.json({
-    message: "data user dengan id " + id + " Telah diubah",
-    name: name,
-    email: email,
-    password: password,
-  });
-});
-
-// METHOD DELETE untuk menghapus user
-app.delete("/api/users/:id", (req, res) => {
-  const { id } = req.params;
-  if (!id) {
-    return res.status(400).json({
-      error: "Silahkan isi field id user!",
-    });
-  }
-
-  res.json({ message: `User dengan ID ${id} telah DIHAPUS!` });
-});
-
-// METHOD GET dengan paramter id
-app.get("/api/users/:id", (req, res) => {
-  const { id } = req.params;
-  if (!id) {
-    return res.status(400).json({
-      error: "Silahkan isi field id user!",
-    });
-  }
-  res.json({
-    data: {
-      name: "Letty",
-      email: "letty@gmail.com",
-      password: "password",
-    },
-  });
+app.use("/api/users", userRoutes);
+app.post("/file-upload", upload.single("file"), [multerError], (req, res) => {
+  res.json({ message: "File uploaded!" });
 });
 
 app.listen(PORT, () =>
